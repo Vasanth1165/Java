5a5fa0cf2b84785b5353c7b5b95c5438611af2f8
Vasanth <86598859+Vasanth1165@users.noreply.github.com>
2022-07-17 16:10:31 +0530

    Create StringAppend.java

---
+++ StringAppend.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class java {
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner (System.in);
+	    String s=sc.next();
+		System.out.println(s);
+		StringBuilder sb=new StringBuilder("");
+		sb.append("i");
+		sb.append("'m");
+		System.out.println(sb);
+	}
+}
