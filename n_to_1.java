fb87c7b80b82f20a0884fca3cc2eae8a087bafc8
Vasanth <86598859+Vasanth1165@users.noreply.github.com>
2022-07-27 10:11:17 +0530

    Create n_to_1.java

---
+++ n_to_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class recursion {
+    
+    public static void printnum(int n){
+        if (n==0){
+            return;
+        }
+        System.out.println(n);
+        printnum(n-1);
+    }
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		printnum(n);
+	}
+}
